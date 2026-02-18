abfc68b1f7bcbfbb835d3a8e45da3b12be05e7ed
mudanya-kijedi <119953313+mudanya-kijedi@users.noreply.github.com>
2026-02-18 12:21:34 +0300

    Initialize server with Express and routes

---
+++ server.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+const authRoutes = require("./routes/authRoutes");
+const taskRoutes = require("./routes/taskRoutes");
+
+const app = express();
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/tasks", taskRoutes);
+
+app.listen(5000, () => console.log("Server running on port 5000"));
